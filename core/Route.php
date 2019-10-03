f7fae67c0abd498e8627e1cc198557c0fa494307
Beatom <beatom@gmail.com>
2019-10-03 16:32:35 +0300

    route check controller and action

--- core/Route.php
+++ core/Route.php
@@ -6,6 +6,39 @@ class Route
 {
     public static function execute()
     {
-        echo 'route'; exit();
+
+        // DEFAULT CONTROLLER AND ACTION
+        $controllerName = 'Main';
+        $actionName = 'Index';
+
+        // PARSE REQUEST
+        $requestParts = explode('/', trim($_SERVER['REQUEST_URI']));
+
+        // CONTROLLER
+        if(!empty($requestParts[1])) {
+            $controllerName = $requestParts[1];
+        }
+
+        // ACTION
+        if(!empty($requestParts[2])) {
+            $actionName = $requestParts[2];
+
+            $params = explode('?', $actionName);
+            if(!empty($params[0])) {
+                $actionName = $params[0];
+            }
+
+            // IF ACTION CONSISTS FROM SEVERAL WORDS, SEPARATED BY DASH '-'
+            $actionParts = explode('-', $actionName);
+            if(count($actionParts) > 1) {
+
+                $parts = [];
+                foreach($actionParts as $part) {
+                    $parts[] = ucfirst($part);
+                }
+
+                $actionName = implode('', $parts);
+            }
+        }
     }
 }
