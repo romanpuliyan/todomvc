318ebb11209e7b54702580ee483b2dc1844fbde9
Beatom <beatom@gmail.com>
2019-10-06 20:45:14 +0300

    form of new item in modal

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -6,7 +6,7 @@
                     <div class="card-body">
                         <h4 class="card-title">Awesome Todo list</h4>
                         <div class="add-items d-flex">
-                            <button class="add btn btn-primary font-weight-bold todo-list-add-btn">Add</button> 
+                            <button class="add btn btn-primary font-weight-bold todo-list-add-btn" data-toggle="modal" data-target="#newItemModal">Add</button>
                         </div>
                         <div class="add-items d-flex"> <input type="text" class="form-control todo-list-input" placeholder="What do you need to do today?">
                             <button class="add btn btn-primary font-weight-bold todo-list-add-btn">Apply</button>
@@ -35,6 +35,36 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="newItemModal" tabindex="-1" role="dialog" aria-labelledby="newItemModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="newItemModalLabel">New Item</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="itemTitle" class="col-form-label">Title:</label>
+                        <input type="text" class="form-control" name="title" id="itemTitle">
+                    </div>
+                    <div class="form-group">
+                        <label for="itemDescription" class="col-form-label">Description:</label>
+                        <textarea class="form-control" name="description" id="itemDescription"></textarea>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
     </div>
