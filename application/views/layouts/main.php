784429eb596498cef74c90a5f8b6350dc20f96c7
Beatom <beatom@gmail.com>
2019-10-06 20:31:57 +0300

    main page new HTML and CSS

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -4,7 +4,15 @@
         <meta charset="utf-8">
         <title>Main page</title>
 
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+        <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+        <script type="text/javascript" src="/js/main.js"></script>
+
     </head>
     <body>
         <?php echo $content; ?>
