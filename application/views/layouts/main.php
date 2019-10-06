9274559a207480353a6fb573cb7111f962d8037a
Beatom <beatom@gmail.com>
2019-10-06 20:03:42 +0300

    main page HTML and CSS

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>Main page</title>
+
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
     </head>
     <body>
         <?php echo $content; ?>
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,1 +1,25 @@
-<h2>Main page</h2>
+<header>
+    <h1>TODO List</h1>
+</header>
+<div id="todo-list">
+    <div id="todo-app">
+        <input type="text" class="new-todo" placeholder="Что хотим сделать?">
+        <ul class="filters-todo">
+            <li><a href="" class="active">Все</a></li>
+            <li><a href="" class="">Активные</a></li>
+            <li><a href="" class="">Закрытые</a></li>
+        </ul>
+        <ul class="list-todo">
+            <li>
+                <input type="checkbox" id="first" class="checkbox" value="on">
+                <label for="first" class="success">second</label>
+                <button class="destroy">✕</button>
+            </li>
+            <li>
+                <input type="checkbox" id="second" class="checkbox" value="on">
+                <label for="second" class="">first</label>
+                <button class="destroy">✕</button>
+            </li>
+        </ul>
+    </div>
+</div>
